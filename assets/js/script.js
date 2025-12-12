5d53bb4162f00822b7aecdd3f7e981f14b17637e
YourGitHubUsername <n.j.thomas1991@gmail.com>
2025-12-12 12:55:30 +0000

    generated random numbers for run game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,17 @@ document.addEventListener("DOMContentLoaded", function() {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked  ${gameType}`);
             }
+        });
+    }
+});
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
+// Generate two random numbers between 1 and 25
+let num1 = Math.floor(Math.random() * 25) + 1;
+let num2 = Math.floor(Math.random() * 25) + 1;
 
 }
 
