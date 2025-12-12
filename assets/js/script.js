1e83f21f4bb4a3716f9cad8f5aaf3f1f23248925
YourGitHubUsername <n.j.thomas1991@gmail.com>
2025-12-12 11:57:11 +0000

    js structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
-console.log("connected!");
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+    
+}        
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+
+function displayAddionQuestion() {
+}
+
+function displaySubtractQuestion() {
+}   
+
+function displayMultiplyQuestion() {    
+}
+
+function displayDivisionQuestion() {    
+}
